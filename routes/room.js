5c286b05ec4f8ed11f323764cdf843b6523928c2
ilhoon <rival6821@naver.com>
2020-07-26 19:22:44 +0900

    room id 없는 경우 처리

--- routes/room.js
+++ routes/room.js
@@ -2,6 +2,11 @@ var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
+router.get("/", function (req, res, next) {
+  backURL = req.header("Referer") || "/";
+  res.redirect(backURL);
+});
+
 router.get("/:id", function (req, res, next) {
   var room = req.params.id;
   res.render("room", { room: room });
