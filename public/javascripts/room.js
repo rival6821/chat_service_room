d1e5a2cdaa571c4c19a564368aff215c589c566a
ilhoon <rival6821@naver.com>
2020-07-29 21:26:27 +0900

    채팅방 별로 접속 관리

--- public/javascripts/room.js
+++ public/javascripts/room.js
@@ -1,1 +1,36 @@
 var socket = io.connect("http://localhost:3000");
+
+var pathArr = location.pathname.split("/");
+var roomName = pathArr[pathArr.length - 1];
+
+function guid() {
+  function s4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return (
+    s4() +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    "-" +
+    s4() +
+    s4() +
+    s4()
+  );
+}
+
+var id = guid();
+
+socket.emit("room enter", roomName, id);
+
+socket.on("room enter", function () {
+  console.log("room enter");
+});
+
+socket.on("disconnect", function () {
+  console.log("disconnect");
+});
--- socket.js
+++ socket.js
@@ -2,13 +2,22 @@ function socket(server) {
   var io = require("socket.io")(server);
 
   io.on("connection", function (socket) {
-    console.log(location);
     console.log("a user connected");
+
+    socket.on("room enter", function (roomName, id) {
+      socket.join(roomName, function () {
+        console.log(`a new user connected ${roomName}`);
+        io.to(roomName).emit("room enter");
+      });
+    });
+
     socket.on("chat message", function (msg) {
       io.emit("chat message", msg);
     });
-    socket.on("disconnect", function () {
-      console.log("user disconnected");
+
+    // 연결종료
+    socket.on("disconnect", function (data) {
+      console.log("a user disconnected");
     });
   });
 }
