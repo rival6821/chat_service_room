748afbe265851e0fafd13316915a85183f0da916
ilhoon <rival6821@naver.com>
2020-08-13 22:02:15 +0900

    소켓

--- socket.js
+++ socket.js
@@ -3,11 +3,13 @@ function socket(server) {
 
   io.on("connection", function (socket) {
     var userId = null;
+    var room = null;
 
     console.log("a user connected");
 
     socket.on("room enter", function (roomName, id) {
       userId = id;
+      room = roomName;
       socket.join(roomName, function () {
         console.log(`a new user connected ${roomName}`);
         io.to(roomName).emit("room enter");
@@ -20,6 +22,7 @@ function socket(server) {
 
     // 연결종료
     socket.on("disconnect", function (data) {
+      socket.leave(room);
       console.log("a user disconnected");
     });
   });
